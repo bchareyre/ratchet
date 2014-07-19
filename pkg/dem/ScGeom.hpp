682baaffcb25836fcc44cdb606c962249c4a2ac9
Bruno Chareyre <bruno.chareyre@grenoble-inp.fr>
2014-07-19 14:13:34 +0200

    Revert "preparation for various algorithms in contact laws"
    This reverts commit bee742349d5b06cf72c9f592ebc0b4d324dc627a.

--- pkg/dem/ScGeom.hpp
+++ pkg/dem/ScGeom.hpp
@@ -8,19 +8,18 @@
 #include<yade/core/State.hpp>
 #include<yade/lib/base/Math.hpp>
 #include<yade/pkg/dem/DemXDofGeom.hpp>
-
 /*! Class representing geometry of two bodies in contact.
  *
+ * The code under SCG_SHEAR is experimental and is used only if ElasticContactLaw::useShear is explicitly true
  */
 
-#define NORATCH2
+#define SCG_SHEAR
 
 class ScGeom: public GenericSpheresContact {
 	private:
 		//cached values
 		Vector3r twist_axis;//rotation vector around normal
 		Vector3r orthonormal_axis;//rotation vector in contact plane
-		enum { OBJECTIVE=1, NORATCHETING=2, MID_STEP_NORMAL=4, EXACT_SHEAR=8, EXACT_ROTATE=16 };
 	public:
 		// inherited from GenericSpheresContact: Vector3r& normal;
 		Real &radius1, &radius2;
@@ -45,50 +44,13 @@ class ScGeom: public GenericSpheresContact {
 		Vector3r getIncidentVel(const State* rbp1, const State* rbp2, Real dt, bool avoidGranularRatcheting=true);
 		// Add function to get the relative angular velocity (useful to determine bending moment at the contact level)
 		Vector3r getRelAngVel(const State* rbp1, const State* rbp2, Real dt);
-
-		// return a vector orthogonal to argument
-		Vector3r orthVect (const Vector3r& v);
-		// rotate a vector using Rodriguez relation. In all cases, axisAngle is NOT a unit vector
-		// passing more arguments favors performance, the functions are in the order of increasing speed
-		Vector3r rrot (const Vector3r& axisAngle, const Vector3r& v) const;
-		Vector3r rrot (const Vector3r& axisAngle, const Real& angle, const Vector3r& v) const;
-		Vector3r rrot (const Vector3r& axisAngle, const Real& angle, const Real& sinA, const Real& cosA, const Vector3r& v) const;
-		Vector3r rrotWithSin (const Vector3r& axisSinAngle, const Vector3r& v) const;
-		Vector3r rrotWithSin (const Vector3r& axisSinAngle, const Real& sinA, const Vector3r& v) const;
-		Vector3r rrotWithSin (const Vector3r& axisSinAngle, const Real& sinA, const Real& cosA, const Vector3r& v) const;
-		
 		
 		// convenience version to be called from python
 		Vector3r getIncidentVel_py(shared_ptr<Interaction> i, bool avoidGranularRatcheting);
 		Vector3r getRelAngVel_py(shared_ptr<Interaction> i);
 
 		YADE_CLASS_BASE_DOC_ATTRS_INIT_CTOR_PY(ScGeom,GenericSpheresContact,"Class representing :yref:`geometry<IGeom>` of a contact point between two :yref:`bodies<Body>`. It is more general than sphere-sphere contact even though it is primarily focused on spheres interactions (reason for the 'Sc' namming); it is also used for representing contacts of a :yref:`Sphere` with non-spherical bodies (:yref:`Facet`, :yref:`Plane`,  :yref:`Box`, :yref:`ChainedCylinder`), or between two non-spherical bodies (:yref:`ChainedCylinder`). The contact has 3 DOFs (normal and 2×shear) and uses incremental algorithm for updating shear.\n\nWe use symbols $\\vec{x}$, $\\vec{v}$, $\\vec{\\omega}$ respectively for position, linear and angular velocities (all in global coordinates) and $r$ for particles radii; subscripted with 1 or 2 to distinguish 2 spheres in contact. Then we define branch length and unit contact normal\n\n.. math::\n\n\tl=||\\vec{x}_2-\\vec{x}_1||, \\vec{n}=\\frac{\\vec{x}_2-\\vec{x}_1}{||\\vec{x}_2-\\vec{x}_1||}\n\nThe relative velocity of the spheres is then\n\n.. math::\n\n\t\\vec{v}_{12}=\\frac{r_1+r_2}{l}(\\vec{v}_2-\\vec{v}_1) -(r_2 \\vec{\\omega}_2 + r_1\\vec{\\omega}_1)\\times\\vec{n}\n\nwhere the fraction multplying translational velocities is to make the definition objective and avoid ratcheting effects (see :yref:`Ig2_Sphere_Sphere_ScGeom.avoidGranularRatcheting`). The shear component is\n\n.. math::\n\n\t\\vec{v}_{12}^s=\\vec{v}_{12}-(\\vec{n}\\cdot\\vec{v}_{12})\\vec{n}.\n\nTangential displacement increment over last step then reads\n\n.. math::\n\n\t\\Delta\\vec{x}_{12}^s=\\Delta t \\vec{v}_{12}^s.",
-		((Real,penetrationDepth,0,(Attr::readonly),"Penetration distance of spheres (positive if overlapping)"))
-		
-		((int,shearScheme,3,,"Selectively define the algorithm to be used for the shear components.\n\n"
-		"== ===============================================================\n"
-		"1  Objective? uses the historical Cundall's expression with constant radius if 0 (discards options 2,4,8), '1' let other options be used.\n"
-		"2  avoid granular ratcheting? (see :yref:`Ig2_Sphere_Sphere_ScGeom::avoidGranularRatcheting`) \n"
-		"4  use mid-step normal instead of current step for the projection of relative velocity \n"
-		"8  compute exact relative shear instead of projecting the shearVelocity*dt. \n"
-		"16  use exact equations for updating contact orientation, gives more precision but takes more time.…\n"
-		"== ===============================================================\n\n"
-		"By default, the mask is 3, using a fast non-ratcheting algorithm, at the price of precision. Other meaningful values are:\n"
-		"0, for experimenting the historical formulation \n"
-		"1, for the most commonly used equations \n"
-		"5, like above but with mid-step normal (as in PFC) \n"
-		"7, non-ratcheting with mid-step normal \n"
-		"11, exact non-ratcheting formulation, approximated contact rotation \n"
-		"16,17,19,21,23,27, exact contact rotation combined with the above. Exact rotations are not really useful without exact shear displacements, since the result would be inexact anyway.\n"
-		"Forbidden/inconsistent: 2,4,8,9,12,14."
-		))
-		#ifdef NORATCH2
-		((Real,penetrationInc,0,(Attr::noSave|Attr::readonly),"Increment of normal displacement between two steps. Used by non-ractcheting code."))
-		((Vector3r,midStepN,Vector3r::Zero(),," - "))
-		((Vector3r,us,Vector3r::Zero(),," - "))
-		((Vector3r,t1,Vector3r::Zero(),," - "))
-		((Real,alpha,1,," - "))
-		#endif
+		((Real,penetrationDepth,NaN,(Attr::noSave|Attr::readonly),"Penetration distance of spheres (positive if overlapping)"))
 		((Vector3r,shearInc,Vector3r::Zero(),(Attr::noSave|Attr::readonly),"Shear displacement increment in the last step"))
 		,
 		/* extra initializers */ ((radius1,GenericSpheresContact::refR1)) ((radius2,GenericSpheresContact::refR2)),
